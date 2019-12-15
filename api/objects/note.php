33ff50ce28547b96734656dc8deb31c2ecb2d00b
Oleg Snegirev(FGL/MARKS) <Oleg.Sniegirev@fglsports.com>
2019-12-14 22:31:41 -0700

    Add search, update, delete & category endpoints

--- api/objects/note.php
+++ api/objects/note.php
@@ -108,5 +108,100 @@ class Note{
     $this->category_name = $row['category_name'];
 }
 
+    // update the note
+    function update(){
+ 
+    // update query
+    $query = "UPDATE
+                " . $this->table_name . "
+            SET
+                name = :name,
+                text = :text,
+                category_id = :category_id
+            WHERE
+                id = :id";
+ 
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->name=htmlspecialchars(strip_tags($this->name));
+    $this->text=htmlspecialchars(strip_tags($this->text));
+    $this->category_id=htmlspecialchars(strip_tags($this->category_id));
+    $this->id=htmlspecialchars(strip_tags($this->id));
+ 
+    // bind new values
+    $stmt->bindParam(':name', $this->name);
+    $stmt->bindParam(':text', $this->text);
+    $stmt->bindParam(':category_id', $this->category_id);
+    $stmt->bindParam(':id', $this->id);
+ 
+    // execute the query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+    }
+
+    // delete the note
+    function delete(){
+ 
+    // delete query
+    $query = "DELETE FROM " . $this->table_name . " WHERE id = ?";
+ 
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->id=htmlspecialchars(strip_tags($this->id));
+ 
+    // bind id of record to delete
+    $stmt->bindParam(1, $this->id);
+ 
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+     
+    }
+
+
+    // search notes
+    function search($keywords){
+ 
+    // select all query
+    $query = "SELECT
+                c.name as category_name, p.id, p.name, p.text, p.category_id, p.created
+            FROM
+                " . $this->table_name . " p
+                LEFT JOIN
+                    categories c
+                        ON p.category_id = c.id
+            WHERE
+                p.name LIKE ? OR p.text LIKE ? OR c.name LIKE ?
+            ORDER BY
+                p.created DESC";
+ 
+    // prepare query statement
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $keywords=htmlspecialchars(strip_tags($keywords));
+    $keywords = "%{$keywords}%";
+ 
+    // bind
+    $stmt->bindParam(1, $keywords);
+    $stmt->bindParam(2, $keywords);
+    $stmt->bindParam(3, $keywords);
+ 
+    // execute query
+    $stmt->execute();
+ 
+    return $stmt;
+}
+
 }
 ?>
